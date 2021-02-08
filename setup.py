70006f3c240901fc32304f560e834b124093f72e
asleep-cult <asleep.cult@gmail.com>
2021-02-07 20:50:22 -0500

    Hopefully this works

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+
+setuptools.setup(
+    name='snakecord',
+    version='0.0.1',
+    url='https://github.com/blanketsucks/snakecord',
+    packages=setuptools.find_packages()
+)
